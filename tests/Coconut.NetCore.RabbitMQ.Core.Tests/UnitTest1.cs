3650711ff259a29e3ec9d5c7282fa2b7c50d379f
Тапканов Марат <marat2785@gmail.com>
2022-04-29 01:21:42 +0300

    divided into different workflows

---
+++ tests/Coconut.NetCore.RabbitMQ.Core.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Coconut.NetCore.RabbitMQ.Core.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
