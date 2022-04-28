0efa3b86ed354b5fd2ac220e416e4af800e7c31b
Тапканов Марат <marat2785@gmail.com>
2022-04-28 19:44:41 +0300

    added event handler

---
+++ src/Coconut.NetCore.RabbitMQ.Core/Handlers/IRabbitMqEventHandler.cs
@@ -0,0 +1,17 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Coconut.NetCore.RabbitMQ.Core.Events;
+
+namespace Coconut.NetCore.RabbitMQ.Core.Handlers
+{
+    /// <summary>
+    ///     RabbitMQ event handler.
+    /// </summary>
+    public interface IRabbitMqEventHandler
+    {
+        /// <summary>
+        ///     Handles the RabbitMQ events.
+        /// </summary>
+        Task Handle(IRabbitMqEvent @event, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Coconut.NetCore.RabbitMQ/Events/ConsumptionEventBase.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Linq;
+using Coconut.NetCore.RabbitMQ.Core.Events;
+using RabbitMQ.Client.Events;
+
+namespace Coconut.NetCore.RabbitMQ.Events
+{
+    /// <summary>
+    ///     RabbitMQ message consumption event.
+    /// </summary>
+    public abstract class ConsumptionEventBase : IRabbitMqEvent
+    {
+        /// <summary>
+        ///     <see cref="BasicDeliverEventArgs"/>
+        /// </summary>
+        /// <value></value>
+        public BasicDeliverEventArgs BasicEvent { get; }
+
+        /// <inheritdoc />
+        public Dictionary<string, object> Data { get; }
+
+        /// <inheritdoc cref="IRabbitMqEvent" />
+        public override string ToString() =>
+            string.Join("; ", Data.Select(x => $"key: {x.Key} Value: {x.Value}"));
+
+        /// <summary>
+        ///     Creates RabbitMQ message consumption event.
+        /// </summary>
+        protected ConsumptionEventBase(BasicDeliverEventArgs basicEvent, Dictionary<string, object> data)
+        {
+            BasicEvent = basicEvent;
+            Data = data ?? new();
+        }
+    }
+}
